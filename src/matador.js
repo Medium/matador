8859de91e8a152a3c85bd476ceeb2fd46487c9fb
Dustin Senos <dustin@dustinsenos.com>
2012-01-06 11:45:23 -0800

    Removed express-hogan and rewrote logic into matador.js. Updated partial support to not double compile them when they are used in both layouts and views

--- src/matador.js
+++ src/matador.js
@@ -1,4 +1,5 @@
 var express = module.exports = require('express')
+  , hogan = require('hogan.js')
   // not all globals are bad
   , app = global.app = express.createServer()
   , Class = global.Class = require('klass')
@@ -10,6 +11,20 @@ app.configure(function () {
 
 
 module.exports.mount = require('./router').init
-module.exports.engine = require('express-hogan.js')
+module.exports.engine = {
+  compile: function(source, options) {
+    if (typeof source != 'string') return source
+    return function (options) {
+      options.locals = options.locals || {}
+      options.partials = options.partials || {}
+      if (options.body) options.locals.body = options.body
+      for (var i in options.partials) {
+        if (v.is.fun(options.partials[i].r)) continue
+        options.partials[i] = hogan.compile(options.partials[i])
+      }
+      return hogan.compile(source, options).render(options.locals, options.partials)
+    }
+  }
+}
 module.exports.BaseController = require('./BaseController')
 module.exports.partials = require('./partialRenderer')
