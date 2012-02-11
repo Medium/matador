c95d56ee9d7977221e4dc677cd076fef41615d77
Kevin Grandon <kevingrandon@yahoo.com>
2012-02-11 08:00:20 +0000

    This commit fixes issue #14
    Implement a few simple methods to copy files using node fs calls instead of exec commands.

--- bin/matador.js
+++ bin/matador.js
@@ -2,14 +2,49 @@
 var fs = require('fs')
   , exec = require('child_process').exec
 
+	/**
+	 * Quick method to copy files
+	 */
+	, cp = function(oldFile, newFile, fn) {
+    var oldFileStream = fs.createReadStream(oldFile)
+    	, newFileStream = fs.createWriteStream(newFile)
+			, fn = fn || function () {
+        console.log('Successfully created ' + newFile)
+      }
+
+		newFileStream.once('open', function(fd){
+      require('util').pump(oldFileStream, newFileStream, fn)
+    })
+	}
+
+	/**
+	 * Quick method to copy files,
+	 * type-R because it's fast
+	 */
+  , cpR = function(source, dest) {
+		// Create the desination directory
+    var checkDir = fs.statSync(source)
+    fs.mkdirSync(dest, checkDir.mode)
+
+    var files = fs.readdirSync(source)
+
+    for(var i = 0; i < files.length; i++) {
+        var thisFile = fs.lstatSync(source + "/" + files[i])
+
+        if(thisFile.isDirectory()) {
+					// Recursive case
+					cpR(source + "/" + files[i], dest + "/" + files[i])
+        } else {
+					// Single file case
+					cp(source + "/" + files[i], dest + "/" + files[i])
+        }
+    }
+}
+
 var methods = {
   init: function (path) {
     console.log('installing Matador into ' + path)
-    fs.mkdirSync('./' + path)
-    exec('cp -R ' + __dirname + '/../src/all/* ' + path + '/.', function (err, out) {
-      if (err) return console.log('error', err)
-      console.log('Success!')
-    })
+    cpR(__dirname + '/../src/all', path)
   }
 , controller: function (name) {
     if (name.match(/([^\/]+)\//)) {
@@ -19,27 +54,25 @@ var methods = {
       copyContents()
     }
     function copyContents() {
-      var destinationFile = './app/controllers/' + name.replace(/(?:[^\/]+)$/, function (m) {
+      var newFile = './app/controllers/' + name.replace(/(?:[^\/]+)$/, function (m) {
         return m.replace(/(.{1})/, function (m, l) {
           return l.toUpperCase()
         })
       }) + 'Controller.js'
-      console.log('generating controller ' + destinationFile)
-      var stub = __dirname + '/../src/StubController.js'
-      exec('cp ' + stub + ' ' + destinationFile, function (er, out) {
-        console.log('Successfully created ' + destinationFile)
-      })
+			, oldFile = __dirname + '/../src/StubController.js'
+      console.log('generating controller ' + newFile)
+
+	    cp(oldFile, newFile)
     }
   }
 , model: function (name) {
     console.log('generating model ' + name)
-    var destinationFile = './app/models/' + name.replace(/^(.{1})/, function (m, l) {
+    var newFile = './app/models/' + name.replace(/^(.{1})/, function (m, l) {
       return l.toUpperCase()
     }) + 'Model.js'
-    var stub = __dirname + '/../src/StubModel.js'
-    exec('cp ' + stub + ' ' + destinationFile, function (er, out) {
-      console.log('Successfully created ' + destinationFile)
-    })
+			, oldFile = __dirname + '/../src/StubModel.js'
+
+    cp(oldFile, newFile)
   }
 }
 !function (args) {
