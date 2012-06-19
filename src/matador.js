744ef4e01f7693b5fc9df7b7d51f8975424b1b4c
Jeremy Stanley <jeremy@obvious.com>
2012-06-19 13:57:55 -0700

    try/catch is unnecessary

--- src/matador.js
+++ src/matador.js
@@ -153,40 +153,36 @@ module.exports.createApp = function (baseDir, configuration, options) {
      * @param {Function} callback
      */
     function getTemplate(templateName, options, callback) {
-      try {
-        // is the template already cached?
-        if (templateCache[templateName]) return callback(null, templateCache[templateName])
+      // is the template already cached?
+      if (templateCache[templateName]) return callback(null, templateCache[templateName])
 
-        // check if it has a suffix already applied
-        var matches = templateName.match(/(\.[\w]+)$/)
-        var suffix
-        if (!matches) {
-          suffix = '.html'
-          templateName += '.html'
-        } else {
-          suffix = matches[0]
-        }
-
-        // is there an engine for the provided suffix?
-        var engine = templateEngines[suffix]
-        if (!engine) return callback(Error("No engine found for template type " + suffix))
-
-        // does the template exist?
-        if (!path.existsSync(templateName)) return callback(new Error("Template '" + templateName + "' does not exist"))
-
-        // read the template in, cache, and call the callback
-        fs.readFile(templateName, 'utf8', function (err, data) {
-          if (err) return callback(err)
-          try {
-            templateCache[templateName] = engine.compile(data, options)
-          } catch (e2) {
-            callback(e2)
-          }
-          return callback(null, templateCache[templateName])
-        })
-      } catch (e) {
-        callback(e)
+      // check if it has a suffix already applied
+      var matches = templateName.match(/(\.[\w]+)$/)
+      var suffix
+      if (!matches) {
+        suffix = '.html'
+        templateName += '.html'
+      } else {
+        suffix = matches[0]
       }
+
+      // is there an engine for the provided suffix?
+      var engine = templateEngines[suffix]
+      if (!engine) return callback(Error("No engine found for template type " + suffix))
+
+      // does the template exist?
+      if (!path.existsSync(templateName)) return callback(new Error("Template '" + templateName + "' does not exist"))
+
+      // read the template in, cache, and call the callback
+      fs.readFile(templateName, 'utf8', function (err, data) {
+        if (err) return callback(err)
+        try {
+          templateCache[templateName] = engine.compile(data, options)
+        } catch (e2) {
+          callback(e2)
+        }
+        return callback(null, templateCache[templateName])
+      })
     }
 
     /**
