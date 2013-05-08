7cc9b4516111a2422579e8e4826a1084fbb45bb3
Vinicius Baggio Fuentes <vinibaggio@gmail.com>
2013-05-08 09:37:54 -0700

    Removing configure() method from app

--- src/all/server.js
+++ src/all/server.js
@@ -8,40 +8,23 @@ var matador = require('matador')
 // Register the matador cache helper.
 app.registerHelper('Cache', matador.helpers.CacheHelper)
 
-app.configure(function () {
+app.set('view engine', 'html')
+app.register('.html', matador.engine)
 
-  app.set('view engine', 'html')
-  app.register('.html', matador.engine)
+// Use the cache helper's no-cache middleware.
+app.use(app.getHelper('Cache').auditHeadersMiddleware)
+app.use(app.getHelper('Cache').noCacheMiddleware)
 
-  // Use the cache helper's no-cache middleware.
-  app.use(app.getHelper('Cache').auditHeadersMiddleware)
-  app.use(app.getHelper('Cache').noCacheMiddleware)
+app.use(matador.cookieParser())
+app.use(matador.session({secret: 'boosh'}))
 
-  app.use(matador.cookieParser())
-  app.use(matador.session({secret: 'boosh'}))
+// TODO: Add JSON body parser middleware
+app.use(app.requestDecorator())
+app.use(app.preRouter())
 
-  // TODO: Add JSON body parser middleware
-  app.use(app.requestDecorator())
-  app.use(app.preRouter())
+app.use(matador.bodyParser())
 
-  app.use(matador.bodyParser())
-})
-
-app.configure('development', function () {
-  app.use(matador.errorHandler({ dumpExceptions: true, showStack: true }))
-  app.set('soy options', {
-    eraseTemporaryFiles: true
-    , allowDynamicRecompile: true
-  })
-})
-
-app.configure('production', function () {
-  app.use(matador.errorHandler())
-})
-
-app.configure(function () {
-  app.use(app.router({}))
-})
+app.use(app.router({}))
 
 app.prefetch()
 app.mount()
