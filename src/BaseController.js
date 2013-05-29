7fa39695466750e1d44fa886c3b54d9523b10481
Vinicius Baggio Fuentes <vinibaggio@gmail.com>
2013-05-29 09:54:55 -0700

    Extracting SoyNode rendering from BaseController

--- src/BaseController.js
+++ src/BaseController.js
@@ -1,14 +1,12 @@
 var fs = require('fs')
   , path = require('path')
-  , soynode = require('soynode')
   , existsSync = fs.existsSync || path.existsSync
+  , SoyNodeTemplateEngine = require('template_engines/SoyNodeTemplateEngine')
 
 module.exports = function (app) {
   var viewCache = {}
     , layoutCache = {}
     , DEFAULT_LAYOUT = 'layout'
-    , DEFAULT_CLOSURE_LAYOUT = 'soy:views.layout.layout'
-    , CLOSURE_LAYOUT_BODY_HTML_KEY = 'bodyHtml'
 
   return klass(function () {
     this._paths = [app.set('base_dir')]
@@ -54,14 +52,12 @@ module.exports = function (app) {
     , render: function (res, view, data, fn, opt_injectedData) {
         data = data || {}
 
-        var prefix = view.substring(0, view.indexOf(':'))
-        if (prefix == 'soy') {
-          // NOTE: The Express template rendering system assumes that all template names
-          // are file names, whereas multiple closure templates exist in a single soy file,
-          // so we short-circuit the rendering framework altogether. The only loss of functionality
-          // is caching, but soynode gives that to us for free.
-          var output = renderClosureTemplate(
-              view.substring(4), data, this.layout, opt_injectedData)
+        var soyNodeTemplateEngine = new SoyNodeTemplateEngine()
+        if (soyNodeTemplateEngine.appliesToTemplate(view)) {
+          var output = soyNodeTemplateEngine.renderTemplate(
+            view, data, this.layout, opt_injectedData
+          )
+
           return fn ? fn(output) : res.send(output)
         }
 
@@ -89,68 +85,6 @@ module.exports = function (app) {
         }
       }
     })
-
-  /**
-   * Strips the soy prefix if found and returns the result.
-   * @param {string} name to be stripped.
-   * @return {string} the original name or stripped result.
-   */
-  function maybeStripSoyPrefix(name) {
-    var prefix = name.substring(0, name.indexOf(':'))
-    return (prefix == 'soy') ? name.substring(4) : name
-  }
-
-  /**
-   * Returns the closure layout template. Note that the layout can be overridden in data.
-   * @param {Object} data the soy data object.
-   * @param {*} the layout requested.
-   */
-  function getClosureLayout(data, layout) {
-    // If an override is specified, then use it.
-    if (typeof data['layout'] !== 'undefined') {
-      layout = data['layout']
-    }
-
-    // If the layout specified is a string, remap the default layout to the Closure version.
-    if (typeof layout === 'string') {
-      layout = (layout === DEFAULT_LAYOUT) ? DEFAULT_CLOSURE_LAYOUT : layout
-    } else if (layout || typeof layout === 'undefined') {
-      layout = DEFAULT_CLOSURE_LAYOUT
-    }
-
-    return layout ? maybeStripSoyPrefix(layout) : layout
-  }
-
-  /**
-   * Renders a closure template that has already been compiled.
-   * @param {string} templateName
-   * @param {Object} data
-   * @param {*} layout
-   * @param {Object=} opt_injectedData optional injected data for $ij
-   */
-  function renderClosureTemplate(templateName, data, layout, opt_injectedData) {
-    var templateFn = soynode.get(templateName)
-    if (!templateFn) {
-      throw new Error('Unable to find template: ' + templateName)
-    }
-
-    var layoutFn = null
-    layout = getClosureLayout(data, layout)
-    if (layout) {
-      layoutFn = soynode.get(layout)
-      if (!layoutFn) {
-        throw new Error('Unable to find layout template: ' + layout)
-      }
-    }
-
-    var ijData = opt_injectedData || {}
-    if (!layoutFn) {
-      return templateFn(data, null, ijData)
-    }
-
-    ijData[CLOSURE_LAYOUT_BODY_HTML_KEY] = templateFn(data, null, ijData)
-    return layoutFn(data, null, ijData)
-  }
 
   /**
    * Searches through a set of paths for a matching view file.
--- src/TemplateEngine.js
+++ src/TemplateEngine.js
@@ -7,10 +7,93 @@
 var fs = require('fs')
   , path = require('path')
   , existsSync = fs.existsSync || path.existsSync
+  , soynode = require('soynode')
+  , DEFAULT_CLOSURE_LAYOUT = 'soy:views.layout.layout'
+  , CLOSURE_LAYOUT_BODY_HTML_KEY = 'bodyHtml'
 
 function TemplateEngine() {
   this._templateCache = {}
   this._templateEngines = {}
+}
+
+function SoyTemplateEngine() {
+}
+
+SoyTemplateEngine.prototype.appliesToTemplate = function (templateName) {
+  var prefix = templateName.substring(0, view.indexOf(':'))
+  return prefix === 'soy'
+}
+
+SoyTemplateEngine.prototype.renderTemplate = function (templateName, data, layout, injectedData) {
+  // NOTE: The Express template rendering system assumes that all template names
+  // are file names, whereas multiple closure templates exist in a single soy file,
+  // so we short-circuit the rendering framework altogether. The only loss of functionality
+  // is caching, but soynode gives that to us for free.
+  return this._renderClosureTemplate(
+    templateName.substring(4), data, layout, injectedData
+  )
+}
+
+/**
+ * Strips the soy prefix if found and returns the result.
+ * @param {string} name to be stripped.
+ * @return {string} the original name or stripped result.
+ */
+SoyTemplateEngine.prototype._maybeStripSoyPrefix = function (name) {
+  var prefix = name.substring(0, name.indexOf(':'))
+  return (prefix == 'soy') ? name.substring(4) : name
+}
+
+/**
+ * Returns the closure layout template. Note that the layout can be overridden in data.
+ * @param {Object} data the soy data object.
+ * @param {*} the layout requested.
+ */
+SoyTemplateEngine.prototype._getClosureLayout = function (data, layout) {
+  // If an override is specified, then use it.
+  if (typeof data['layout'] !== 'undefined') {
+    layout = data['layout']
+  }
+
+  // If the layout specified is a string, remap the default layout to the Closure version.
+  if (typeof layout === 'string') {
+    layout = (layout === DEFAULT_LAYOUT) ? DEFAULT_CLOSURE_LAYOUT : layout
+  } else if (layout || typeof layout === 'undefined') {
+    layout = DEFAULT_CLOSURE_LAYOUT
+  }
+
+  return layout ? this._maybeStripSoyPrefix(layout) : layout
+}
+
+/**
+ * Renders a closure template that has already been compiled.
+ * @param {string} templateName
+ * @param {Object} data
+ * @param {*} layout
+ * @param {Object=} opt_injectedData optional injected data for $ij
+ */
+SoyTemplateEngine.prototype._renderClosureTemplate = function (templateName, data, layout, opt_injectedData) {
+  var templateFn = soynode.get(templateName)
+  if (!templateFn) {
+    throw new Error('Unable to find template: ' + templateName)
+  }
+
+  var layoutFn = null
+  layout = this._getClosureLayout(data, layout)
+  if (layout) {
+    layoutFn = soynode.get(layout)
+    if (!layoutFn) {
+      throw new Error('Unable to find layout template: ' + layout)
+    }
+  }
+
+  var ijData = opt_injectedData || {}
+  if (!layoutFn) {
+    return templateFn(data, null, ijData)
+  }
+
+  ijData[CLOSURE_LAYOUT_BODY_HTML_KEY] = templateFn(data, null, ijData)
+  return layoutFn(data, null, ijData)
 }
 
 /**
