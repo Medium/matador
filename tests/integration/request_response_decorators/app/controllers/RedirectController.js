c09dd648e7c1c79279eafbdff8a0087fc4ca3dea
Vinicius Baggio Fuentes <vinibaggio@gmail.com>
2013-05-28 19:19:27 -0700

    Adding test for response.redirect() decorator

---
+++ tests/integration/request_response_decorators/app/controllers/RedirectController.js
@@ -0,0 +1,23 @@
+var util = require('util')
+
+module.exports = function (app, config) {
+  var ApplicationController = app.getController('Application', true)
+
+  /** @constructor */
+  function HomeController() {
+    ApplicationController.call(this)
+  }
+
+  util.inherits(HomeController, ApplicationController)
+
+  HomeController.prototype.redirect = function (req, res) {
+    res.redirect('/target')
+  }
+
+  HomeController.prototype.target = function (req, res) {
+    res.statusCode = 200
+    res.end('Success!')
+  }
+
+  return HomeController
+}
