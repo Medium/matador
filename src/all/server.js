5caf80d021789f79abd22e5e195dd3e5d0ac9f89
Vinicius Baggio Fuentes <vinibaggio@gmail.com>
2013-06-11 18:25:55 -0700

    app.configure is not necessary #jankAndDrank

--- src/all/server.js
+++ src/all/server.js
@@ -8,20 +8,18 @@ var matador = require('matador')
 // Register the matador cache helper.
 app.registerHelper('Cache', matador.helpers.CacheHelper)
 
-app.configure(function () {
-  // Use the cache helper's no-cache middleware.
-  app.use(app.getHelper('Cache').auditHeadersMiddleware)
-  app.use(app.getHelper('Cache').noCacheMiddleware)
+// Use the cache helper's no-cache middleware.
+app.use(app.getHelper('Cache').auditHeadersMiddleware)
+app.use(app.getHelper('Cache').noCacheMiddleware)
 
-  app.use(matador.cookieParser())
-  app.use(matador.session({secret: 'boosh'}))
+app.use(matador.cookieParser())
+app.use(matador.session({secret: 'boosh'}))
 
-  // TODO: Add JSON body parser middleware
-  app.use(app.requestDecorator())
-  app.use(app.preRouter())
+// TODO: Add JSON body parser middleware
+app.use(app.requestDecorator())
+app.use(app.preRouter())
 
-  app.use(matador.bodyParser())
-})
+app.use(matador.bodyParser())
 
 app.configure('development', function () {
   app.use(matador.errorHandler({ dumpExceptions: true, showStack: true }))
