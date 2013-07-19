bcc889ddd7f8e27eb836c775e6ef65e2580f4053
Vinicius Baggio Fuentes <vinibaggio@gmail.com>
2013-07-19 10:35:52 -0700

    Creating test case for model loading

---
+++ tests/functional/app/models/TestModel.js
@@ -0,0 +1,5 @@
+module.exports = function (app, config) {
+  var Test = function () {}
+  Test.test = true
+  return Test
+}
---
+++ tests/functional/model_loading_test.js
@@ -0,0 +1,14 @@
+// Copyright The Obvious Corporation 2013
+
+require('../support/functional')
+
+var matador = require('../../src/matador')
+
+exports.testModelsAreLoaded = function (test) {
+  var app = matador.createApp(__dirname, {}, {})
+    , model = app.getModel('Test', true)
+
+  test.ok(model.call && model.apply)
+  test.ok(model.test)
+  test.done()
+}
